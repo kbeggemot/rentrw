cda2924cdf6186206c69094448295cc0101a7bea
kbeggemot <kbeggemot@gmail.com>
2025-08-24 15:39:35 +0300

    logo

--- src/components/BrandMark.tsx
+++ src/components/BrandMark.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export function BrandMark({ className = '' }: { className?: string }) {
+export function BrandMark({ className = '', size = 80 }: { className?: string; size?: number }) {
   return (
     <div className={`flex flex-col items-center ${className}`}>
-      <img src="/logo.svg" alt="Logo" className="block h-12 w-auto" />
+      <img src="/logo.svg" alt="Logo" className="block" style={{ height: size, width: 'auto' }} />
     </div>
   );
 }
