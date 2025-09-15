26ac124804b262cf05b1fe8b89e521631e0f7a99
kbeggemot <kbeggemot@gmail.com>
2025-09-15 13:50:25 +0300

    admin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,6 @@
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-6xl mx-auto p-4">
+    <div className="w-screen mx-0 p-2">
       {children}
     </div>
   );
