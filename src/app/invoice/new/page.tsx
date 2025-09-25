b9c5d55e74e68d8f4abb61daf9b837bbae9ced27
kbeggemot <kbeggemot@gmail.com>
2025-09-25 18:43:50 +0300

    Update page.tsx

--- src/app/invoice/new/page.tsx
+++ src/app/invoice/new/page.tsx
@@ -215,7 +215,7 @@ export default function InvoiceNewPage() {
   const showLogin = useMemo(() => !phone, [phone]);
 
   return (
-    <div className="max-w-xl mx-auto pt-8 pb-4">
+    <div className="max-w-xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">Создать счёт</h1>
 
       {showLogin ? (
